5a25654bc375bf7ee60eb76ef71f49cebafb7026
Law Guidote <ldaguidote@gmail.com>
2025-01-31 03:16:09 +0800

    Update utils.py
    Created functions sort_and_filter_by_case_type, convert_df_to_json_list_v2, restructure_analysis

--- llm_generator/prompt/utils.py
+++ llm_generator/prompt/utils.py
@@ -22,7 +22,7 @@ def filter_dataset(df, **kwargs):
         else:
             df = df[df[k] == v]
     return df
-
+   
 
 def filter_by_case_type(df, case_type):
 
@@ -60,6 +60,7 @@ def resample_data(df, n_examples):
 
     return df
 
+
 def convert_df_to_json_list(df):
     json_list = []
     for _, row in df.iterrows():
@@ -87,3 +88,79 @@ def convert_df_to_json_list(df):
     return json_list
 
 
+def sort_and_filter_by_case_type(df, case_type):
+    case_dict = {
+        'Misrepresentation': 'misrepresentation',
+        'Negative Behaviour': 'negative_aspects',
+        'Due Prominence': 'omit_due_prominence',
+        'Generalisation': 'generalisation',
+        'Imagery and Headlines': 'headline_bias'
+    }
+    if case_type in case_dict.keys():
+        case_type = case_dict[case_type]
+        df = df[df.article_id.duplicated() == False]
+        sorted_df =  df.sort_values([case_type, 'bias_rating', 'publish_date'], ascending=False)
+        filtered_df = sorted_df[sorted_df[case_type] != "NA"].head()
+        return filtered_df
+
+    else:
+        error_clause = "Case type must be in this list: 'Biased', 'Very Biased', 'Misrepresentation', " \
+            "'Negative Behaviour', 'Due Prominence', 'Generalisation', and 'Imagery and Headlines'."
+        raise ValueError(error_clause)
+
+    
+def convert_df_to_json_list_v2(df, case_type):
+    case_dict = {
+        'Misrepresentation': 'misrepresentation',
+        'Negative Behaviour': 'negative_aspects',
+        'Due Prominence': 'omit_due_prominence',
+        'Generalisation': 'generalisation',
+        'Imagery and Headlines': 'headline_bias'
+    }
+    case_type = case_dict[case_type]
+
+    json_list = []
+    for _, row in df.iterrows():
+        row_dict = dict()
+        row_dict['headline'] = row['headline']
+        row_dict['bias_category'] = case_type
+        row_dict['bias_category_score'] = row[case_type]
+        row_dict['analysis'] = row[f'{case_type}_analysis']
+        row_dict['bias_rating'] = row['bias_rating']
+
+        json_list.append(row_dict)
+
+    return json_list
+
+
+def restructure_analysis(analysis, case_type):
+    """Remove details of the analysis that is not relevant to the case type"""
+
+    category_map = {
+        'Generalisation': 'Category 1',
+        'Negative Behaviour': 'Category 2',
+        'Misrepresentation': 'Category 3',
+        'Due Prominence': 'Category 4',
+        'Imagery and Headlines': 'Category 5'
+        }
+    category = category_map[case_type]
+
+    allowed_sections = ['# Executive Summary', '# Analysis', '# Overall Assessment', '# Recommendations'] + ['## ' + category]
+    
+    tag_list = []
+    analysis_lines = []
+    analysis = analysis.split('\n')
+    
+    for line in analysis:
+        if len(line) > 0:
+            if line[0] == '#':
+                tag = line
+        else:
+            pass
+
+        tag_list.append(tag)
+        if any(list(map(lambda x: x in tag, allowed_sections))):
+            analysis_lines.append(line)
+
+    restructured_analysis = '\n'.join(analysis_lines)
+    return restructured_analysis
